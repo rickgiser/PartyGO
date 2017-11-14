ba23e8b5fc00b5f7897e1d2f90f14e87fa5952ec
Rick Bing <bingqx1993@gmail.com>
2017-11-14 21:30:33 +0800

    增加mint-ui

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Mint);
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +15,4 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
-})
+});
