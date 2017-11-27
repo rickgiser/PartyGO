c767c6f96fd03731069b14467a30d9675d5caaf0
ChenWangshan <1178980144@qq.com>
2017-11-27 22:00:53 +0800

    完成高德搜索服务的调用

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,12 @@
 <template>
-  <div id="home">
-    <div class="amap-page-container" id="home_map">
-      <el-amap ref="map" vid="amapDemo" :amap-manager="amapManager" :center="center" :zoom="zoom" :plugin="plugin" :events="events" class="amap-demo">
-      </el-amap>
-    </div>
-    <div class="search">
-      <input type="text" placeholder="搜地点、查公交、找路线" class="search_input"></input>
+  <div id="home" class="m-map" >
+    <div class="search" v-if="placeSearch">
+      <input type="text" placeholder="搜地点、查公交、找路线" class="search_input" v-model="searchKey"></input>
       <img src="../assets/defavatar.png" alt="" class="search_img">
-      <span class="search_button"><i class="fa fa-search fa-lg" aria-hidden="true"></i></span>
-      <div id="js-result" class="search_result"></div>
+      <span class="search_button" @click="handleSearch"><i class="fa fa-search fa-lg" aria-hidden="true"></i></span>
+      <div id="js-result" class="search_result" v-show="searchKey"></div>
     </div>
+    <div id="js-container" class="map"></div>
   </div>
 </template>
 
@@ -65,57 +62,104 @@
       .search_result{
         max-height: 300px; 
         overflow: auto; 
-        margin-top: 10px;
+        margin-top: 37px;
       }
+    }
+    .map{
+      height:100%;
     }
   }
 </style>
 
 
 <script>
-  import AMap from 'vue-amap';
-  let amapManager = new AMap.AMapManager();
-  export default{
-    data: function() {
-      return {
-        amapManager,
-        zoom: 12,
-        center: [121.59996, 31.197646],
-        events: {
-          init: (o) => {
-            console.log(o.getCenter())
-            console.log(this.$refs.map.$$getInstance())
-            o.getCity(result => {
-              console.log(result)
-            })
-          },
-          'moveend': () => {
-          },
-          'zoomchange': () => {
-          },
-          'click': (e) => {
-            // alert('map clicked');
-          }
-        },
-        plugin: ['ToolBar', {
-          pName: 'MapType',
-          defaultType: 0,
-          events: {
-            init(o) {
-              console.log(o);
-            }
-          }
-        }]
-      };
-    },
-
-    methods: {
-      getMap() {
-        // amap vue component
-        console.log(amapManager._componentMap);
-        // gaode map instance
-        console.log(amapManager._map);
+import remoteLoad from '@/utils/remoteLoad.js'
+import { MapKey, MapCityName } from '@/config/config'
+export default {
+  props: ['lat', 'lng'],
+  data () {
+    return {
+      searchKey: '',
+      placeSearch: null,
+      dragStatus: false,
+      AMapUI: null,
+      AMap: null
+    }
+  },
+  watch: {
+    searchKey () {
+      if (this.searchKey === '') {
+        this.placeSearch.clear()
       }
     }
+  },
+  methods: {
+    // 搜索
+    handleSearch () {
+      if (this.searchKey) {
+        this.placeSearch.search(this.searchKey)
+      }
+    },
+    // 实例化地图
+    initMap () {
+      // 加载PositionPicker，loadUI的路径参数为模块名中 'ui/' 之后的部分
+      let AMapUI = this.AMapUI = window.AMapUI
+      let AMap = this.AMap = window.AMap
+      AMapUI.loadUI(['misc/PositionPicker'], PositionPicker => {
+        let mapConfig = {
+          zoom: 16,
+          cityName: MapCityName
+        }
+        if (this.lat && this.lng) {
+          mapConfig.center = [this.lng, this.lat]
+        }
+        let map = new AMap.Map('js-container', mapConfig)
+        // 加载地图搜索插件
+        AMap.service('AMap.PlaceSearch', () => {
+          this.placeSearch = new AMap.PlaceSearch({
+            pageSize: 5,
+            pageIndex: 1,
+            citylimit: false,
+            // city: MapCityName,
+            map: map,
+            panel: 'js-result'
+          })
+        })
+        // 启用工具条
+        AMap.plugin(['AMap.ToolBar'], function () {
+          map.addControl(new AMap.ToolBar({
+            position: 'RB'
+          }))
+        })
+        // 创建地图拖拽
+        let positionPicker = new PositionPicker({
+          mode: 'dragMap', // 设定为拖拽地图模式，可选'dragMap'、'dragMarker'，默认为'dragMap'
+          map: map // 依赖地图对象
+        })
+        // 拖拽完成发送自定义 drag 事件
+        positionPicker.on('success', positionResult => {
+          // 过滤掉初始化地图后的第一次默认拖放
+          if (!this.dragStatus) {
+            this.dragStatus = true
+          } else {
+            this.$emit('drag', positionResult)
+          }
+        })
+        // 启动拖放
+        positionPicker.start()
+      })
+    }
+  },
+  async created () {
+    // 已载入高德地图API，则直接初始化地图
+    if (window.AMap && window.AMapUI) {
+      this.initMap()
+    // 未载入高德地图API，则先载入API再初始化
+    } else {
+      await remoteLoad('http://webapi.amap.com/maps?v=1.3&key=39a902a2bc6a0bb933de87db7d588a4e')
+      await remoteLoad('http://webapi.amap.com/ui/1.0/main.js')
+      this.initMap()
+    }
   }
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import MintUI from 'mint-ui'
-import AMap from 'vue-amap'
+// import AMap from 'vue-amap'
 
 Vue.config.productionTip = false;
 Vue.use(MintUI);
-Vue.use(AMap);
-AMap.initAMapApiLoader({
+// Vue.use(AMap);
+/* AMap.initAMapApiLoader({
   key: '39a902a2bc6a0bb933de87db7d588a4e',
   plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor']
-});
+}); */
 
 /* eslint-disable no-new */
 new Vue({
